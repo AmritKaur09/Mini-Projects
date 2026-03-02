ee03d6e5944eaba59f0224e76372876917c5d8b8
amrit <amarjeetkaurkhalsa54@gmail.com>
2026-03-02 22:15:57 +0530

    Cleaned repo to recommit files properly

--- StudentRecords/student_functions.c
+++
@@ -1,9 +0,0 @@
-#include "student_functions.h"
-
-void calculateAverage(Student *s) {
-    float sum = 0;
-    for(int i = 0; i < MAX_SUBJECTS; i++) {
-        sum += s->marks[i];
-    }
-    s->average = sum / MAX_SUBJECTS;
-}
